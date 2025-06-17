a7b671e966593ad6675d4f379629ff7a789e4a05
Filbert <filbertayo09@gmail.com>
2025-06-17 12:28:44 +0300

    asdsa

--- resources/views/vendor/Chatify/layouts/info.blade.php
+++ resources/views/vendor/Chatify/layouts/info.blade.php
@@ -1,22 +1,24 @@
-   @php
-       $chatUser = \App\Models\User::find($id); // $id is the ID of the other user in the chat
-   @endphp
+  @php
+    $chatUser = \App\Models\User::find(request()->route('id'));
+@endphp
 
 @if ($chatUser)
-   <div class="text-center mb-3">
-       <img src="{{ asset($chatUser->file ?? 'image/prof.jpeg') }}" alt="User Image" class="img-fluid rounded-circle"
-           width="100" height="100">
-   </div>
-       @endif
+    <div class="text-center mb-3">
+        <img src="{{ asset($chatUser->file ?? 'image/prof.jpeg') }}"
+             alt="User Image"
+             class="img-fluid rounded-circle"
+             width="100" height="100">
+    </div>
+@endif
 
-   <p class="info-name text-center">{{ config('chatify.name') }}</p>
+<p class="info-name text-center">{{ $chatUser->name ?? config('chatify.name') }}</p>
 
-   <div class="messenger-infoView-btns text-center">
-       <a href="#" class="danger delete-conversation">Delete Conversation</a>
-   </div>
+<div class="messenger-infoView-btns text-center">
+    <a href="#" class="danger delete-conversation">Delete Conversation</a>
+</div>
 
-   {{-- shared photos --}}
-   <div class="messenger-infoView-shared">
-       <p class="messenger-title"><span>Shared Photos</span></p>
-       <div class="shared-photos-list"></div>
-   </div>
+{{-- shared photos --}}
+<div class="messenger-infoView-shared">
+    <p class="messenger-title"><span>Shared Photos</span></p>
+    <div class="shared-photos-list"></div>
+</div>
