3736b9b9f998f93a4fc3839963ea95a940db807d
Filbert <filbertayo09@gmail.com>
2025-06-17 11:38:41 +0300

    ada

--- resources/views/vendor/Chatify/layouts/info.blade.php
+++ resources/views/vendor/Chatify/layouts/info.blade.php
@@ -1,12 +1,22 @@
-{{-- user info and avatar --}}
-{{-- <div class="avatar av-l chatify-d-flex"></div> --}}
- <img src="{{ asset(Auth::user()->file ?? 'image/prof.jpeg') }}" alt="User Image"
-                                class="img-fluid rounded-circle" width="200" height="200">
+  @php
+    $chatUser = \App\Models\User::find(request()->route('id'));
+@endphp
 
-<p class="info-name">{{ config('chatify.name') }}</p>
-<div class="messenger-infoView-btns">
+@if ($chatUser)
+    <div class="text-center mb-3">
+        <img src="{{ asset($chatUser->file ?? 'image/prof.jpeg') }}"
+             alt="User Image"
+             class="img-fluid rounded-circle"
+             width="100" height="100">
+    </div>
+@endif
+
+<p class="info-name text-center">{{ $chatUser->name ?? config('chatify.name') }}</p>
+
+<div class="messenger-infoView-btns text-center">
     <a href="#" class="danger delete-conversation">Delete Conversation</a>
 </div>
+
 {{-- shared photos --}}
 <div class="messenger-infoView-shared">
     <p class="messenger-title"><span>Shared Photos</span></p>
